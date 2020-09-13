ebccc8689ff5862ddec6c3d629669fc3b78cbb06
HyunJoon,Kim <kimkuma04@gmail.com>
2020-09-13 13:46:48 +0900

    Api update, Basetime add.

--- src/test/java/com/kuma/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/kuma/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.kuma.springboot.domain.posts;
 
+import org.assertj.core.api.LocalDateTimeAssert;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -38,6 +40,21 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
 
+    @Test
+    public void BasicTimeEntity_등록() {
+
+        LocalDateTime now = LocalDateTime.of(2019,6,4,0,0,0);
+        postsRepository.save(Posts.builder().title("title").content("content").author("author").build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>> createDate = " + posts.getCreateDate()+", modifiedDate="+posts.getModifiedDate());
+
+        assertThat(posts.getCreateDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
